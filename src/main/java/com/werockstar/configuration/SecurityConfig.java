5bc1adc97c218786c0324d72af45cd492660f8e6
WeRockStar <kotchaphan.m@gmail.com>
2017-01-29 22:14:59 +0700

    Add sample security config

---
+++ src/main/java/com/werockstar/configuration/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.werockstar.configuration;
+
+
+import com.werockstar.ReaderRepository;
+import com.werockstar.ReadingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ReaderRepository readerRepository;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/readingList/**")
+                .access("hasRole('READER')")
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true")
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                UserDetails userDetails = readerRepository.findOne(username);
+                if (userDetails != null)
+                    return userDetails;
+                throw new UsernameNotFoundException("User : " + username + " not found");
+            }
+        };
+    }
+}
