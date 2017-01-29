9a0b51edb6d8f02008d3f18c4a7c49c32bf0e686
WeRockStar <kotchaphan.m@gmail.com>
2017-01-29 18:37:39 +0700

    Add command explain annotation spring boot application

--- src/main/java/com/werockstar/ReadingListApplication.java
+++ src/main/java/com/werockstar/ReadingListApplication.java
@@ -3,6 +3,10 @@ package com.werockstar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	@SpringBootApplication
+	Enable component-scanning and Auto configuration
+ */
 @SpringBootApplication
 public class ReadingListApplication {
 
