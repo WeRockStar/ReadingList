a2c02e19255cdfec0050c46f67a2bc4856a3e435
WeRockStar <kotchaphan.m@gmail.com>
2017-01-29 21:45:20 +0700

    Prepare spring security

---
+++ src/main/java/com/werockstar/domain/Reader.java
@@ -0,0 +1,77 @@
+package com.werockstar.domain;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+public class Reader implements UserDetails {
+
+    private static final Long serialVersionId = 1L;
+
+    @Id
+    private String username;
+    private String fullName;
+    private String password;
+
+    public static Long getSerialVersionId() {
+        return serialVersionId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("READER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
