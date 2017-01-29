81aca93666020e53a4423566c7b77cf8e8c7c81d
WeRockStar <kotchaphan.m@gmail.com>
2017-01-29 23:07:35 +0700

    Sample configuration properties

--- src/main/java/com/werockstar/controller/ReadingListController.java
+++ src/main/java/com/werockstar/controller/ReadingListController.java
@@ -1,9 +1,11 @@
 package com.werockstar.controller;
 
 
+import com.werockstar.AmazonProperties;
 import com.werockstar.domain.Book;
 import com.werockstar.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +16,16 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix = "amazon")
 public class ReadingListController {
 
+    private AmazonProperties amazonProperties;
     private ReadingListRepository readingListRepository;
 
     @Autowired
-    public ReadingListController(ReadingListRepository readingListRepository) {
+    public ReadingListController(ReadingListRepository readingListRepository, AmazonProperties amazonProperties) {
         this.readingListRepository = readingListRepository;
+        this.amazonProperties = amazonProperties;
     }
 
     @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
@@ -28,6 +33,8 @@ public class ReadingListController {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", amazonProperties.getAssociateId());
         }
 
         return "readingList"; // templates/readingList.html
@@ -37,6 +44,6 @@ public class ReadingListController {
     public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
-        return "redirect:/{reader}";
+        return "redirect:/";
     }
 }
